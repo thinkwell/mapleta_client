321246f558c75cc375877da6a71c37e93b3abe1c
Brandon Turner <bturner@bltweb.net>
2011-05-04 17:01:22 -0500

    Refactored Assignment to use new HashInitialize functionality

--- lib/mapleta/assignment.rb
+++ lib/mapleta/assignment.rb
@@ -4,11 +4,36 @@ require 'logger'
 module Maple::MapleTA
   class Assignment
     include HashInitialize
-    attr_accessor :classId, :id, :name, :class_name, :mode, :modeDescription, :passingScore, :totalPoints, :weight, :start, :end, :timeLimit, :policy
+    property :class_id,         :type => :integer,      :from => :classId
+    property :id,               :type => :integer
+    property :class_name
+    property :name, :default => nil
+    property :mode, :type => :integer, :default => nil
+    property :mode_description, :from => :modeDescription, :default => nil
+    property :passing_score, :type => :integer, :from => :passingScore, :default => nil
+    property :total_points, :type => :float, :from => :totalPoints, :default => nil
+    property :weight, :type => :float, :default => nil
+    property :start, :type => :time_from_ms, :default => nil
+    property :end, :type => :time_from_ms, :default => nil
+    property :time_limit, :type => :integer, :from => :timeLimit, :default => nil
+    property :policy, :default => nil
+
+    MODE_PROCTERED_TEST     = 0
+    MODE_UNPROCTERED_TEST   = 1
+    MODE_PRACTICE           = 2
+    MODE_MASTERY_ASSIGNMENT = 3
+    MODE_STUDY_SESSION      = 4
+
+
+    def load(connection)
+      defaults!
+      connection.ws.assignment(self)
+    end
+    alias :reload :load
 
 
     def launch(connection, external_data=nil, view_opts={})
-      raise Errors::MapleTAError, "Connection class id (#{connection.class_id}) doesn't match assignment class id (#{self.classId})" unless self.classId.to_i == connection.class_id.to_i
+      raise Errors::MapleTAError, "Connection class id (#{connection.class_id}) doesn't match assignment class id (#{self.class_id})" unless self.class_id == connection.class_id.to_i
 
       params = {
         'wsExternalData' => external_data,
@@ -22,10 +47,21 @@ module Maple::MapleTA
 
 
     def post(connection, url, data, view_opts={})
-      raise Errors::MapleTAError, "Connection class id (#{connection.class_id}) doesn't match assignment class id (#{self.classId})" unless self.classId.to_i == connection.class_id.to_i
+      raise Errors::MapleTAError, "Connection class id (#{connection.class_id}) doesn't match assignment class id (#{self.class_id})" unless self.class_id == connection.class_id.to_i
 
       page(connection.fetch_page(url, data, :post), connection, view_opts)
     end
+
+
+    def timed?
+      time_limit > 0 && ![MODE_PRACTICE, MODE_STUDY_SESSION].include?(mode)
+    end
+
+
+    def recorded?
+      [MODE_PROCTERED_TEST, MODE_UNPROCTERED_TEST, MODE_MASTERY_ASSIGNMENT].include?(mode)
+    end
+
 
 
   private
