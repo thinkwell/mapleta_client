75088f2b745a632f45c0418364cb8c8cfe2b9713
Brandon Turner <bturner@bltweb.net>
2011-06-29 10:48:04 -0500

    Added support for parsing the progress table

--- lib/mapleta_client/page/mastery_question.rb
+++ lib/mapleta_client/page/mastery_question.rb
@@ -40,6 +40,15 @@ module Page
     end
 
 
+    def total_questions
+      if is_progress_report?
+        progress_report.length
+      else
+        false
+      end
+    end
+
+
     def progress_report_node
       unless @progress_report_node
         @progress_report_node = page.parser.at_xpath('.//em[text()="Progress Report"]').next_element rescue -1
@@ -50,6 +59,52 @@ module Page
 
     def progress_report_html
       progress_report_node ? progress_report_node.to_xhtml : ''
+    end
+
+
+    # Returns an array representing the progress report.  For example:
+    #
+    #   [
+    #     {:title => 'Fraction Arithmetic', :requirement => :met,   :correct => 1, :incorrect => 0, :attempts => 1},
+    #     {:title => 'Factorization',       :requirement => :begun, :correct => 0, :incorrect => 2, :attempts => 2},
+    #     {:title => 'Easy Addition',       :requirement => false,  :correct => 0, :incorrect => 0, :attempts => 0},
+    #   ]
+    def progress_report
+      return [] unless progress_report_node
+
+      unless @progress_report_data
+        @progress_report_data = []
+        progress_report_node.xpath('./tr').each do |tr|
+          next if tr['class'] && tr['class'].include?('head')
+
+          row = {}
+          row[:title] = tr.at_xpath('./td[2]').text
+
+          status_class = tr.at_xpath('./td[3]')['class']
+          if status_class && status_class.include?('mastery_requirement_met')
+            row[:requirement] = :met
+          elsif status_class && status_class.include?('mastery_requirement_begun')
+            row[:requirement] = :begun
+          else
+            row[:requirement] = false
+          end
+
+          if tr.at_xpath('./td[4]').text =~ /(\d+)\/(\d+)/
+            row[:correct] = $1.to_i
+            row[:attempts] = $2.to_i
+            row[:incorrect] = row[:attempts] - row[:correct]
+          end
+
+          @progress_report_data << row
+        end
+      end
+      @progress_report_data
+    end
+
+
+    def progress_mastery
+      return nil unless progress_report_node
+      @progress_mastery ||= progress_report_node.at_xpath('./tr[last()]/th[4]').text
     end
 
   end
