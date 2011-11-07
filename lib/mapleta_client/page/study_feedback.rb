1d9e217b47abdae8fcd7b2630bed7c5f811e7971
Brandon Turner <bt@brandonturner.net>
2011-11-07 11:56:36 -0600

    Add correct/incorrect CSS class to feedback pages

--- lib/mapleta_client/page/study_feedback.rb
+++ lib/mapleta_client/page/study_feedback.rb
@@ -8,6 +8,30 @@ module Page
     end
 
 
+    private
+
+    def mandatory_fixes
+      add_wrapper_div
+    end
+
+
+    def add_wrapper_div
+      new_node = @page.parser.create_element 'div'
+      new_node['class'] = 'feedback'
+      content_node.children.each do |node|
+        if node.name == "h3"
+          if node.text =~ /Correct/
+            new_node['class'] += ' correct'
+          elsif node.text =~ /Incorrect/
+            new_node['class'] += ' incorrect'
+          end
+        end
+        node.parent = new_node
+      end
+      content_node.add_child new_node
+    end
+
+
     def validate
       node = @page.parser
       content_node  or raise Errors::UnexpectedContentError.new(node, "Cannot find page content")
