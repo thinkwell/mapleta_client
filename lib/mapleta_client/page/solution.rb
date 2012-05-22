2cfaea70b592dcc358c44d0a34d972e949926502
Pavol Chalupka <paloc@thinkwell.com>
2012-05-22 11:17:49 -0500

    Add wrapper div around solution content

--- lib/mapleta_client/page/solution.rb
+++ lib/mapleta_client/page/solution.rb
@@ -8,6 +8,20 @@ module Page
       @content_node ||= @page.parser.at_css('div.announce')
     end
 
+    private
+
+    def mandatory_fixes
+      add_wrapper_div
+    end
+
+    def add_wrapper_div
+      new_node = @page.parser.create_element 'div'
+      new_node['class'] = 'solution'
+      content_node.children.each do |node|
+        node.parent = new_node
+      end
+      content_node.add_child new_node
+    end
 
     def validate
       node = @page.parser
