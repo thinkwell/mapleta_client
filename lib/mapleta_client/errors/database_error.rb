7665640ee186ce73af469251c88c8675987b92df
Brandon Turner <bt@brandonturner.net>
2012-07-30 12:06:00 -0500

    Add original_error on DatabaseError

--- lib/mapleta_client/errors/database_error.rb
+++ lib/mapleta_client/errors/database_error.rb
@@ -1,7 +1,12 @@
 module Maple::MapleTA
   module Errors
     class DatabaseError < MapleTAError
+      attr_reader :original_error
 
+      def initialize(msg=nil, original_error=nil)
+        @original_error = original_error
+        super(msg.nil? ? original_error.message : msg)
+      end
     end
   end
 end
