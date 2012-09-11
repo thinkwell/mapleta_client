c68c0b822bc910c13f7910621db568b249f89860
Brandon Turner <bt@brandonturner.net>
2012-09-11 17:23:07 -0500

    Add db macro for retrieving test record

--- lib/mapleta_client/database/macros/test_record.rb
+++ lib/mapleta_client/database/macros/test_record.rb
@@ -3,9 +3,13 @@ module Maple::MapleTA
   module Database::Macros
     module TestRecord
 
-      def destroy_test_record(try_id)
+      def test_record(try_id)
         raise Errors::DatabaseError.new("Must pass try_id") unless try_id
-        test_record = exec("SELECT * FROM testrecord WHERE id=$1", [try_id]).first
+        exec("SELECT * FROM testrecord WHERE id=$1", [try_id]).first
+      end
+
+      def destroy_test_record(try_id)
+        test_record = test_record(try_id)
         raise Errors::DatabaseError.new("Cannot find test record with id=#{try_id}") unless test_record && test_record['id']
         transaction do
           exec("DELETE FROM answersheetitem_grade WHERE answersheetitemid IN (SELECT id FROM answersheetitem WHERE testrecordid=$1)", [try_id])
