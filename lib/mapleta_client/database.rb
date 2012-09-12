527bd17381c2fefa351a0fd76a1a425987c2710b
Brandon Turner <bt@brandonturner.net>
2012-09-11 19:30:51 -0500

    Add support for explictly setting the Maple database timezone

--- lib/mapleta_client/database.rb
+++ lib/mapleta_client/database.rb
@@ -10,12 +10,23 @@ module Maple::MapleTA
     @database_config = config
   end
 
+  def self.database_timezone
+    @database_timezone ||= ActiveSupport::TimeZone.create('UTC')
+  end
+
+  def self.database_timezone=(tz)
+    tz = ActiveSupport::TimeZone.create(tz) unless tz.is_a?(ActiveSupport::TimeZone)
+    @database_timezone = tz
+  end
+
   def self.establish_database_connection(*args)
     self.database_config = args unless args.empty?
     begin
       remove_database_connection
       opts = self.database_config.is_a?(Array) ? self.database_config : [self.database_config]
       @database_connection = Maple::MapleTA::Database::Connection.new(*opts)
+      @database_connection.exec("SET TIMEZONE='#{self.database_timezone.name}'")
+      @database_connection
     rescue PG::Error => e
       raise Errors::DatabaseError.new(nil, e)
     end
