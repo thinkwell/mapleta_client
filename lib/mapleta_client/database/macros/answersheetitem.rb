7e15fbc8eb67c4804213e579d01382a4d7597475
dominiksteiner <dominik.j.steiner@gmail.com>
2013-07-25 15:53:18 -0600

    73564 : return count of query

--- lib/mapleta_client/database/macros/answersheetitem.rb
+++ lib/mapleta_client/database/macros/answersheetitem.rb
@@ -6,8 +6,10 @@ module Maple::MapleTA
       def answer_mathml(offset, min_length, condition)
         raise Errors::DatabaseError.new("Must pass offset") unless offset
         raise Errors::DatabaseError.new("Must pass min_length") unless min_length
-        result = exec("select searchableresponsestring from answersheetitem  where searchableresponsestring like '%math%' and searchableresponsestring not like '%mstyle%' and char_length(searchableresponsestring) > $1 #{condition} order by id asc limit 1 offset $2", [min_length, offset]).first
-        result ? Maple::MapleTA::Page::BaseQuestion.convert_to_presentation_mathml(result['searchableresponsestring']).gsub("\n", "") : nil
+        where = "where searchableresponsestring like '%math%' and searchableresponsestring not like '%mstyle%' and char_length(searchableresponsestring) > $1 #{condition}"
+        count = exec("select count(*) from answersheetitem #{where}", [min_length]).first
+        result = exec("select searchableresponsestring from answersheetitem #{where} order by id asc limit 1 offset $2", [min_length, offset]).first
+        result ? [count['count'], Maple::MapleTA::Page::BaseQuestion.convert_to_presentation_mathml(result['searchableresponsestring']).gsub("\n", "")] : nil
       end
 
     end
