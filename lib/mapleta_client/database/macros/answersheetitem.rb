19af5a2bd87abe3e4dc6b01d1a3e4a0c208f5675
dominiksteiner <dominik.j.steiner@gmail.com>
2013-07-25 15:31:23 -0600

    73564 : add condition parameter

--- lib/mapleta_client/database/macros/answersheetitem.rb
+++ lib/mapleta_client/database/macros/answersheetitem.rb
@@ -3,11 +3,11 @@ module Maple::MapleTA
   module Database::Macros
     module Answersheetitem
 
-      def answer_mathml(offset, min_length)
+      def answer_mathml(offset, min_length, condition)
         raise Errors::DatabaseError.new("Must pass offset") unless offset
         raise Errors::DatabaseError.new("Must pass min_length") unless min_length
-        result = exec("select searchableresponsestring from answersheetitem  where searchableresponsestring like '%math%' and searchableresponsestring not like '%mstyle%' and char_length(searchableresponsestring) > $1 order by id asc limit 1 offset $2", [min_length, offset]).first
-        result ? result['searchableresponsestring'] : nil
+        result = exec("select searchableresponsestring from answersheetitem  where searchableresponsestring like '%math%' and searchableresponsestring not like '%mstyle%' and char_length(searchableresponsestring) > $1 #{condition} order by id asc limit 1 offset $2", [min_length, offset]).first
+        result ? Maple::MapleTA::Page::BaseQuestion.convert_to_presentation_mathml(result['searchableresponsestring']).gsub("\n", "") : nil
       end
 
     end
