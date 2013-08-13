c71f84ccfb95a6649b5015508c8e25e081b696ac
dominiksteiner <dominik.j.steiner@gmail.com>
2013-08-12 22:12:56 -0600

    73575 : add questions_for_class method

--- lib/mapleta_client/database/macros/classes.rb
+++ lib/mapleta_client/database/macros/classes.rb
@@ -8,6 +8,15 @@ module Maple::MapleTA
         clazz = exec("DELETE FROM classes WHERE cid=$1", [cid]).first
         [user_class && user_class['id'].to_i, clazz && clazz['id'].to_i]
       end
+
+      ##
+      # Get the class database row for a given classid
+      def class(classid)
+        raise Errors::DatabaseError.new("Must pass classid") unless classid
+        exec("SELECT * FROM classes WHERE cid=$1", [classid]).first
+      end
+
+
     end
   end
 end
--- lib/mapleta_client/database/macros/question.rb
+++ lib/mapleta_client/database/macros/question.rb
@@ -6,6 +6,11 @@ module Maple::MapleTA
         raise Errors::DatabaseError.new("Must pass assignment_class_id") unless assignment_class_id
         exec("Select q.* from question q left join assignment_question_group_map m on m.questionid = q.id left join assignment_question_group a on a.id = m.groupid left join assignment_class c on c.assignmentid = a.assignmentid where c.classid=$1 and q.latestrevision is null", [assignment_class_id])
       end
+
+      def questions_for_class(classid)
+        raise Errors::DatabaseError.new("Must pass classid") unless classid
+        exec("Select q.* from question q left join classes c on c.cid = q.author or c.parent = q.author where c.cid=$1 and q.latestrevision is null", [classid])
+      end
     end
   end
 end
