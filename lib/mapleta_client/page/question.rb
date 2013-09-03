bccddb13f33fb4c169a70b6d487612ed3c00d363
dominiksteiner <dominik.j.steiner@gmail.com>
2013-09-03 06:31:45 -0600

    73593 : applying base_question html fixes to question preview

--- lib/mapleta_client/page/question.rb
+++ lib/mapleta_client/page/question.rb
@@ -1,15 +1,36 @@
 module Maple::MapleTA
 module Page
 
-  class Question < Base
+  class Question < BaseQuestion
 
     # Returns true if the given page looks like a page this class can parse
     def self.detect(page)
       !page.parser.at_css('input[@type="hidden"][name="questionId"]').nil?
     end
 
+    def fix_html
+      fix_equation_entry_mode_links
+      fix_equation_editor
+      fix_text_equation_null_value
+      fix_image_position
+      fix_preview_links
+      fix_plot_links
+      fix_help_links
+      remove_preview_links
+      remove_plot_links
+      remove_equation_editor_label
+    end
+
     def content_node
       @content_node ||= @page.parser.at_css('div[@class="questionstyle"]')
+    end
+
+    def form_node
+      content_node
+    end
+
+    def question_node
+      content_node
     end
 
     def validate
