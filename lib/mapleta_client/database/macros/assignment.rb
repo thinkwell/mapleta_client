bc8871b2692f35684e80bb6586d122194eb973de
macario <mail@makarius.me>
2014-03-19 17:00:56 -0600

    unified create and edit assignment methods WARNING: reliing on sequel/master, need to update as soon as it fixes bug

--- lib/mapleta_client/database/macros/assignment.rb
+++ lib/mapleta_client/database/macros/assignment.rb
@@ -108,76 +108,56 @@ module Maple::MapleTA
         raise Errors::DatabaseError.new(nil, e)
       end
 
-      def create_assignment(assignment)
-        assignment.assignment_class_attributes = {
-          :name         => assignment.name,
-          :total_points => assignment.total_points,
-          :weighting    => assignment.weighting,
-          :assignment_policy_attributes => {
-            :show_current_grade        => assignment.show_current_grade,
-            :insession_grade           => assignment.insession_grade,
-            :reworkable                => assignment.reworkable,
-            :printable                 => assignment.printable,
-            :mode                      => assignment.mode || 0,
-            :show_final_grade_feedback => assignment.show_final_grade_feedback
-          }
+      def save_assignment(assignment)
+        assignment_class_attrs = {
+          :classid     => assignment.classid,
+          :name        => assignment.name,
+          :totalpoints => assignment.total_points,
+          :weighting   => assignment.weighting,
         }
 
-        # One question group (thus one question group map) per question
-        assignment.assignment_question_groups_attributes =
-          assignment.questions.map { |question| 
-            {
-              :name => question.name,
-              :order_id => 0,
-              :assignment_question_group_maps_attributes => [
-                {:question_id => question.id, :question_uid => question.uid}
-              ]
-            }
-          }
+        if assignment.assignment_class
+          assignment_class_attrs[:id] = assignment.assignment_class.id
+        end
 
-        assignment.save
-      end
+        assignment.assignment_class_attributes = assignment_class_attrs
 
-      def edit_assignment(assignment)
-        transaction do
-          assignment.assignment_class_attributes = {
-            :id          => assignment.assignment_class.id,
-            :classid     => assignment.assignment_class.classid,
-            :name        => assignment.name,
-            :totalpoints => assignment.total_points,
-            :weighting   => assignment.weighting,
-          }
+        assignment.assignment_class.assignment_policy_attributes = {
+          :assignment_class_id       => assignment.assignment_class.try(:id),
+          :show_current_grade        => assignment.show_current_grade,
+          :insession_grade           => assignment.insession_grade,
+          :reworkable                => assignment.reworkable,
+          :printable                 => assignment.printable,
+          :mode                      => assignment.mode || 0,
+          :show_final_grade_feedback => assignment.show_final_grade_feedback
+        }
 
-          assignment.assignment_class.assignment_policy_attributes = {
-            :assignment_class_id       => assignment.assignment_class.id,
-            :show_current_grade        => assignment.show_current_grade,
-            :insession_grade           => assignment.insession_grade,
-            :reworkable                => assignment.reworkable,
-            :printable                 => assignment.printable,
-            :mode                      => assignment.mode || 0,
-            :show_final_grade_feedback => assignment.show_final_grade_feedback
-          }
+        question_group_attrs = []
 
+        unless assignment.new?
           question_ids = assignment.questions.map(&:id)
           group_ids    = assignment.assignment_question_group_maps_dataset.select_hash(:questionid, :groupid)
           deletable_question_ids = group_ids.keys - question_ids
-
-          question_group_attrs =
-            assignment.questions.map do |question| 
-              attrs = {
-                :name => question.name, :order_id => 0,
-                :assignment_question_group_maps_attributes => [ {:question_id => question.id, :question_uid => question.uid} ]
-              }
-              attrs[:id] = id if id = group_ids[question.id]
-              attrs
-            end
-
-          question_group_attrs += deletable_question_ids.map { |qid| { :id => group_ids[qid], :_delete => true } }
-          assignment.assignment_question_groups_attributes = question_group_attrs
-          
-          assignment.save.reload
+          question_group_attrs   = deletable_question_ids.map { |qid| { :id => group_ids[qid], :_delete => true } }
         end
+
+        question_group_attrs += assignment.questions.map do |question| 
+          attrs = {
+            :name => question.name, :order_id => 0,
+            :assignment_question_group_maps_attributes => [ {:question_id => question.id, :question_uid => question.uid} ]
+          }
+          attrs[:id] = id if id = group_ids[question.id] unless assignment.new?
+          attrs
+        end
+
+        assignment.assignment_question_groups_attributes = question_group_attrs
+
+        assignment.save.reload
       end
+
+      alias create_assignment save_assignment
+      alias edit_assignment save_assignment
+
 
       def copy_assignment_to_class(assignment_class_id, new_class_id)
         assignment_class = AssignmentClass.with_pk!(assignment_class_id)
