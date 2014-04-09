6f3ff6327cf6fe6936ebe7f314ff40418135d077
Pavol Chalupka <paloc@thinkwell.com>
2014-04-09 12:09:12 -0500

    1.8.7 compatibility modifications.

--- lib/mapleta_client/orm/assignment_question_group_map.rb
+++ lib/mapleta_client/orm/assignment_question_group_map.rb
@@ -1,7 +1,7 @@
 module Maple::MapleTA
   module Orm
     class AssignmentQuestionGroupMap < Sequel::Model( Maple::MapleTA.database_connection.dataset[:assignment_question_group_map] )
-      many_to_one :assignment_question_group, key: :groupid
+      many_to_one :assignment_question_group, :key => :groupid
       # many_to_one :question, key: :questionid
 
       def_column_alias :question_id, :questionid
