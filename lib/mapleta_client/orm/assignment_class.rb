ed8322c40730f74ee17e218613e8a9d2ff5acd03
Pavol Chalupka <paloc@thinkwell.com>
2014-04-11 12:36:38 -0500

    Added copy method to AssignmentClass.

--- lib/mapleta_client/orm/assignment_class.rb
+++ lib/mapleta_client/orm/assignment_class.rb
@@ -26,6 +26,42 @@ module Maple::MapleTA
         self.order_id ||=
           self.class.dataset.where(:classid => class_id).max(:order_id).to_i + 1
       end
+
+      def copy(options = {})
+        db.transaction do
+
+          new_class_id = options[:class_id] || self.class_id
+          new_assignment_class =
+            self.deep_dup :assignment_policy, :assignment => {
+              :assignment_question_groups => :assignment_question_group_maps
+            }
+
+          new_assignment_class.class_id = new_class_id
+
+          new_assignment_name = options[:name] || self.name
+          new_assignment_name = new_assignment_name + ' - COPY' if new_class_id == self.class_id && options[:name].nil?
+          new_assignment_class.name = new_assignment_class.assignment.name = new_assignment_name
+
+          new_start_time = options[:start_time]
+          new_start_time = self.assignment_policy.start_time if new_class_id == self.class_id && !options.has_key?(:start_time)
+          new_assignment_class.assignment_policy.start_time = new_start_time
+
+          new_end_time = options[:end_time]
+          new_end_time = self.assignment_policy.end_time if new_class_id == self.class_id && !options.has_key?(:end_time)
+          new_assignment_class.assignment_policy.end_time = new_end_time
+
+          new_assignment_class.assignment.class_id = new_class_id
+          new_assignment_class.assignment.uid = "#{UUID.new.generate.to_s}-#{new_class_id}"
+          new_assignment_class.save
+
+          if adv_policy = self.advanced_policy
+            new_adv_policy_attrs = adv_policy.to_hash.merge(:assignment_class_id => new_assignment_class.id)
+            new_assignment_class.advanced_policy = AdvancedPolicy.new(new_adv_policy_attrs).save
+          end
+
+          new_assignment_class
+        end
+      end
     end
   end
 end
