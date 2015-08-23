73878aec97800407d6a82f2f75341ef8b8e26094
Pavol Chalupka <paloc@thinkwell.com>
2015-08-23 18:16:10 -0500

    Use existing BaseQuestion for Question preview.

--- lib/mapleta_client/page/question.rb
+++ lib/mapleta_client/page/question.rb
@@ -1,22 +1,13 @@
 module Maple::MapleTA
 module Page
 
-  class Question < Base
+  class Question < BaseQuestion
 
     # Returns true if the given page looks like a page this class can parse
     def self.detect(page)
       !page.parser.at_css('input[@type="hidden"][name="questionId"]').nil?
     end
 
-    def content_node
-      @content_node ||= @page.parser.at_css('div[@class="questionstyle"]')
-    end
-
-    def validate
-      node = @page.parser
-      content_node or raise Errors::UnexpectedContentError.new(node, "Cannot find page content")
-      true
-    end
   end
 
 end
