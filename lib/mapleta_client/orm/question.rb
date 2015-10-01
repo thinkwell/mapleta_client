7b3e79e6dd7e232a092a7b6a66105eb0bab0ad6f
Pavol Chalupka <paloc@thinkwell.com>
2015-10-01 15:55:02 -0500

    Added latest_revision methods to Question.

--- lib/mapleta_client/orm/question.rb
+++ lib/mapleta_client/orm/question.rb
@@ -6,6 +6,14 @@ module Maple::MapleTA
 
       has_many :assignment_question_group_maps, :class_name => 'Maple::MapleTA::Orm::AssignmentQuestionGroupMap', :foreign_key => 'questionid'
 
+      def latest_revision_id
+        latestrevision || self.id
+      end
+
+      def is_latest_revision?
+        !latestrevision
+      end
+
     end
   end
 end
